b13a51f77b08e6f07de6223bc66816e70bb08128
UeberallGebannt <paulmertens63@gmail.com>
2020-12-02 11:36:26 +0100

    Add a simple API

--- SimpleMOTD/src/net/kettlemc/simplemotd/SimpleMOTD.java
+++ SimpleMOTD/src/net/kettlemc/simplemotd/SimpleMOTD.java
@@ -3,6 +3,7 @@ package net.kettlemc.simplemotd;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.kettlemc.simplemotd.api.API;
 import net.kettlemc.simplemotd.configuration.Configuration;
 import net.kettlemc.simplemotd.listener.PlayerJoinListener;
 
@@ -10,9 +11,12 @@ public class SimpleMOTD extends JavaPlugin {
 
 	private Configuration config;
 
+	private static API api;
+
 	public void onEnable() {
 		registerListener();
 		this.config = new Configuration();
+		api = new API(this);
 	}
 
 	private void registerListener() {
@@ -21,6 +25,10 @@ public class SimpleMOTD extends JavaPlugin {
 
 	public Configuration getConfiguration() {
 		return this.config;
+	}
+
+	public static API getAPI() {
+		return api;
 	}
 
 }
---
+++ SimpleMOTD/src/net/kettlemc/simplemotd/api/API.java
@@ -0,0 +1,55 @@
+package net.kettlemc.simplemotd.api;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import net.kettlemc.simplemotd.SimpleMOTD;
+import net.md_5.bungee.api.ChatColor;
+
+public class API {
+
+	private SimpleMOTD plugin;
+
+	public API(SimpleMOTD plugin) {
+		this.plugin = plugin;
+	}
+
+	// Sends the MOTD to a player with a delay
+
+	public void sendMOTD(Player player, int delay) {
+		Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, new Runnable() {
+
+			@Override
+			public void run() {
+				for (String message : plugin.getConfiguration().getMotd())
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', message.replace("%player%", player.getName())));
+			}
+
+		}, delay);
+	}
+
+	// Sends the MOTD to a player
+
+	public void sendMOTD(Player player) {
+		sendMOTD(player, 0);
+	}
+
+	// Returns a list of all motd lines (without replaced placeholders)
+
+	public List<String> getMOTDList() {
+		return plugin.getConfiguration().getMotd();
+	}
+
+	// Returns a the motd message (without replaced placeholders)
+
+	public String getMOTD() {
+		StringBuilder sb = new StringBuilder();
+		for (String message : getMOTDList()) {
+			sb.append(message + "\n");
+		}
+		return ChatColor.translateAlternateColorCodes('&', sb.toString());
+	}
+
+}
